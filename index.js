2a3125af8742349c28dad7668ddf7bbf638daaea
khadija-edris <118714830+khadija-edris@users.noreply.github.com>
2022-11-21 22:53:17 +0200

    Change style and change selected property. position x the value we get x + pixel

--- index.js
+++ index.js
@@ -2,4 +2,7 @@ const btnEL = document.querySelector(".btn");
 btnEL.addEventListener("mouseover", (Event) => {
     const x = (Event.pageX  - btnEL.offsetleft);
     const y = (Event.pageY  - btnEL.offsetleft);
+
+    btnEL.style.setproperty("--xPos", x + "px");
 }
+
